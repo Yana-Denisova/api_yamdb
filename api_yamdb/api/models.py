55d8ceab827fa6ccef9eb0d8fce797b48fe8cb71
atuktanov <atuktanov@yandex.ru>
2022-02-28 22:33:16 +0300

    Начальный вариант

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    USER = 'USER'
+    MODERATOR = 'MODERATOR'
+    ADMIN = 'ADMIN'
+    ROLES = (
+        (USER, 'user'),
+        (MODERATOR, 'moderator'),
+        (ADMIN, 'admin'),
+    )
+    username = models.CharField(
+        max_length=150, validators=[RegexValidator(regex=r'^[\w.@+-]+$')],
+        unique=True, blank=False, null=False)
+    email = models.EmailField(
+        max_length=254, unique=True, blank=False, null=False)
+    bio = models.TextField(max_length=500, blank=True, verbose_name='О себе')
+    role = models.CharField(
+        max_length=50, choices=ROLES,
+        default=USER, verbose_name='Роль')
+
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN or self.is_staff
+
+    @property
+    def is_moderator(self):
+        return self.role == self.MODERATOR or self.is_superuser
